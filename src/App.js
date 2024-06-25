b6cdb9c40aaf58174435246ad1686c2de7a9f9b0
TIvers <wlad2000vita@gmail.com>
2024-06-25 15:02:26 +0700

    Создание пользователя

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CatalogPage from './pages/CatalogPage';
 import AddProductPage from './pages/AddProductPage';
@@ -9,158 +8,123 @@ import ProductDetail from './components/ProductDetail';
 import Sidebar from './components/Sidebar';
 import './index.css';
 
-const products = [
-  {
-    id: 1,
-    image: 'Часы наручные',
-    name: 'Часы наручные и Гелик',
-    price: 100000000000,
-    oldPrice: 1200000000000,
-    description: 'Часы наручные',
-    terms: 'Оплата в течение 3-5 дней после публикации отзыва',
-    availableDay: 'Today',
-    marketPrice: 1500000000000,
-    yourPrice: 130000000,
-    discount: 1500000000000,
-    step: "2",
-  },
-  {
-    id: 2,
-    image: 'path_to_image',
-    name: 'Часы наручные',
-    price: 10300,
-    oldPrice: 12200,
-    description: 'Часы наручные',
-    terms: 'Оплата в течение 3-5 дней после публикации отзыва',
-    availableDay: 'Today',
-    marketPrice: 1500,
-    yourPrice: 1300,
-    discount: 15,
-    step: "Завершено",
-    isComplete: true,
-  },
-  {
-    id: 3,
-    image: 'path_to_image',
-    name: 'Часы наручные',
-    price: 1010000,
-    oldPrice: 1220000,
-    description: 'Часы наручные',
-    terms: 'Payment details',
-    availableDay: 'Today',
-    marketPrice: 1500,
-    yourPrice: 1300,
-    step: "6",
-    discount: 125,
-  },
-];
-const App = () => {
-  const [products, setProducts] = useState([
-    {
-      id: 1,
-      image: 'Часы н33аручные 33ару33ару33ару',
-      name: 'Часы н1231аручные и Гелик 33ару33ару33ару33ару',
-      description: 'Часы наручные 33ару33ару33ару33ару33ару33ару',
-      terms: 'Оплата в течение 3-5 дней после публикации отзыва 33ару33ару33арувавававававававава33ару',
-      availableDay: 'Today 33ару33ару33ару33ару33ару33ару33ару33ару33ару',
-      marketPrice: 1500000000000,
-      yourPrice: 130000000,
-      discount: 1500000000000,
-      step: "2",
-    },
-    {
-      id: 2,
-      image: 'path_t211232o_image',
-      name: 'Часы наручны2222е',
-      description: 'Часы наручные',
-      terms: 'Оплата в течение 3222-54444 дней после публикации отзыва',
-      availableDay: 'Today',
-      marketPrice: 1500,
-      yourPrice: 1300,
-      discount: 15,
-      step: "Завершено",
-      isComplete: true,
-    },
-    {
-      id: 3,
-      image: 'path_t343434o_image',
-      name: 'Часы наручные111111',
-      description: 'Часы наручные',
-      terms: 'Payment323232 details',
-      availableDay: 'To3232323day',
-      marketPrice: 152232300,
-      yourPrice: 1311100,
-      step: "6",
-      discount: 1225,
-    },
-  ]);
 
-  const categories = ['Категория 1', 'Категория 2', 'Категория 3'];
 
-const App = () => {
-  const [userInfo, setUserInfo] = useState(null);
-  const baseURL = 'https://nilurl.ru:8000/'
-  const API = {
-    async getUser(id) {
-      try {
-        const option = {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        };
-        const res = await fetch(`${baseURL}`, option).then(res => res.json());
-        return res;
-      } catch (err) {
-        console.log(err);
-      }
-    },
-    async createUser(id) {
-      try {
-        const option = {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ id: id }),
-        };
-        const res = await fetch(`${baseURL}`, option).then(res => res.json());
-        return res;
-      } catch (err) {
-        console.log(err);
-      }
-    },
-  };
+  const App = () => {
+    const [products, setProducts] = useState([
+      {
+        id: 1,
+        image: 'Часы н33аручные 33ару33ару33ару',
+        name: 'Часы н1231аручные и Гелик 33ару33ару33ару33ару',
+        description: 'Часы наручные 33ару33ару33ару33ару33ару33ару',
+        terms: 'Оплата в течение 3-5 дней после публикации отзыва 33ару33ару33арувавававававававава33ару',
+        availableDay: 'Today 33ару33ару33ару33ару33ару33ару33ару33ару33ару',
+        marketPrice: 1500000000000,
+        yourPrice: 130000000,
+        discount: 1500000000000,
+        step: "2",
+      },
+      {
+        id: 2,
+        image: 'path_t211232o_image',
+        name: 'Часы наручны2222е',
+        description: 'Часы наручные',
+        terms: 'Оплата в течение 3222-54444 дней после публикации отзыва',
+        availableDay: 'Today',
+        marketPrice: 1500,
+        yourPrice: 1300,
+        discount: 15,
+        step: "Завершено",
+        isComplete: true,
+      },
+      {
+        id: 3,
+        image: 'path_t343434o_image',
+        name: 'Часы наручные111111',
+        description: 'Часы наручные',
+        terms: 'Payment323232 details',
+        availableDay: 'To3232323day',
+        marketPrice: 152232300,
+        yourPrice: 1311100,
+        step: "6",
+        discount: 1225,
+      },
+    ]);
+  
+    const categories = ['Категория 1', 'Категория 2', 'Категория 3'];
 
-  useEffect(() => {
-    window.Telegram.WebApp.expand();
-
-    const fetchData = async () => {
-      try {
-        console.log("123");
-        console.log(window.Telegram.WebApp.initDataUnsafe.user.id);
-        console.log(window.Telegram.WebApp.initDataUnsafe.user.username);
-        console.log("12dsadadasdas");
-        const response = await API.getUser(window.Telegram.WebApp.initDataUnsafe.user.id);
-        
-        if (response.data.length === 0) {
-          const createResponse = await API.createUser(window.Telegram.WebApp.initDataUnsafe.user.id);
-          if (createResponse.success === true) {
-            const newUserResponse = await API.getUser(window.Telegram.WebApp.initDataUnsafe.user.id);
-            setUserInfo(newUserResponse.data[0]);
-          }
-        } else {
-          setUserInfo(response.data[0]);
+    const [userInfo, setUserInfo] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    
+    const baseURL = 'https://nilurl.ru:8000/'
+    const API = {
+      async getUser(id) {
+        try {
+          const option = {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          };
+          const res = await fetch(`${baseURL}getUser.php?id=${id}`, option).then(res => res.json());
+          return res;
+        } catch (err) {
+          console.log(err);
         }
-      } catch (e) {
-        console.log(e);
-      }
+      },
+      async createUser(id, username) {
+        try {
+          const option = {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ id: id, username: username }),
+          };
+          const res = await fetch(`${baseURL}createUser.php`, option).then(res => res.json());
+          return res;
+        } catch (err) {
+          console.log(err);
+        }
+      },
     };
-
-    fetchData();
-  }, []);
-
-  if (!userInfo) return null;
+  
+    useEffect(() => {
+      window.Telegram.WebApp.expand();
+    
+      const fetchData = async () => {
+        try {
+          const userId = window.Telegram.WebApp.initDataUnsafe.user.id;
+          const username = window.Telegram.WebApp.initDataUnsafe.user.username;
+          const response = await API.getUser(userId);
+          console.log("этап1");
+          if (!response.success) {
+            const createResponse = await API.createUser(userId, username);
+            if (createResponse.success === true) {
+              const newUserResponse = await API.getUser(userId);
+              setUserInfo(newUserResponse.data);
+              setIsLoading(false);
+              console.log("этап2");
+              console.log(isLoading);
+              console.log(newUserResponse.data);
+            }
+          } else {
+            console.log("этап3");
+            setUserInfo(response.data);
+            setIsLoading(false);
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      };
+    
+      fetchData();
+    }, []);
+  
+    if (isLoading) {
+      console.log("этап0");
+      return <div>Загрузка...</div>;
+  }
 
   return (
     <Router>
