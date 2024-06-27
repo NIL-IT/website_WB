9f9b38bdf22990a304508684257653501f9fe743
Ilya Tetyakov <tetyakov2@gmail.com>
2024-06-27 23:23:03 +0700

    Вернул App

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ import './index.css';
  
 
   const App = () => {
-   // const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
 
-     const [products, setProducts] = useState([
+   /*  const [products, setProducts] = useState([
      {
        id: 1,
        image: "/Canada.jpg" ,
@@ -205,10 +205,10 @@ import './index.css';
                           keywords: "Диффузор для дома, Диффузоры РФ, Домашний ремонт окон и всего возможного",
                           article: "123456",
                           category: "Мужчинам"
-                        },
+                        }, 
                         
 
-   ]); 
+   ]);  */
     const categories = ['Женщинам', 'Мужчинам', 'Обувь', 'Детям', 'Дом', 'Новый год'];
     const [userInfo, setUserInfo] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
@@ -274,7 +274,7 @@ import './index.css';
       }
     };
   
-   /* useEffect(() => {
+    useEffect(() => {
       window.Telegram.WebApp.expand();
   
       const fetchData = async () => {
@@ -305,7 +305,7 @@ import './index.css';
   
     if (isLoading) {
       return <div>Загрузка...</div>;
-    }  */
+    }  
   
     const handleStepComplete = (step, formData) => {
       // Логика для обработки завершения шага
