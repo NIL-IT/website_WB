5703a0a4acb7c8134734f88d60d4180b08889a2e
TIvers <wlad2000vita@gmail.com>
2025-08-11 17:05:43 +0700

    Изменение текста уведомления о сроках выкупа товара на странице шагов покупки

--- src/pages/PurchaseStepsPage.js
+++ src/pages/PurchaseStepsPage.js
@@ -607,7 +607,7 @@ const PurchaseStepsPage = ({
                 товара.
               </p>
               <p className="purchase-step-text">
-                Товар следует выкупить до {
+                Товар следует выкупить и оформить на него отзыв до {
                   (() => {
                     const now = new Date();
                     now.setMonth(now.getMonth() + 3);
